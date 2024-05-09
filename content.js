5ad95a00489bafc91869ac176d393850159e89a4
김애리 <zjadofl0408@gmail.com>
2024-05-09 22:29:31 +0900

    [design] 기본적인 UI 구성

--- content.js
+++ content.js
@@ -1,4 +1,16 @@
-function getYouTubeVideoCurrentTime(button) {
+function formatVideoTime(videoSeconds) {
+  const hours = Math.floor(videoSeconds / 3600);
+  const minutes = Math.floor((videoSeconds % 3600) / 60);
+  const seconds = Math.floor(videoSeconds % 60);
+
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+function getVideoCurrentTime(button) {
   chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
     let results = await chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
@@ -13,12 +25,60 @@ function getYouTubeVideoCurrentTime(button) {
       }
     });
 
-    const currentTimeList = document.getElementById('bookmark_list');
+    const bookmarkList = document.getElementById('bookmark_list');
     for (let result of results) {
       if (result.result !== null) {
         const listItem = document.createElement('li');
-        listItem.textContent = `현재 시간: ${result.result} 초`;
-        currentTimeList.appendChild(listItem);
+        listItem.style.display = "flex";
+        bookmarkList.appendChild(listItem);
+        listItem.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+        const videoInfoDiv = document.createElement("div");
+        listItem.appendChild(videoInfoDiv);
+
+        const memo = document.createElement('input');
+        memo.type = "text";
+        memo.placeholder = "빈 메모입니다.";
+        memo.disabled = true;
+        memo.style.paddingBottom = "10px";
+        memo.style.border = "none";
+        memo.style.background = "transparent";
+        videoInfoDiv.appendChild(memo);
+
+        const time = document.createElement("span");
+        time.textContent = `${formatVideoTime(result.result)}`;
+        time.style.color = "#b8860b";
+        time.style.borderWidth = "2px";
+        time.style.fontSize = "14px";
+        videoInfoDiv.appendChild(time);
+
+        const iconDiv = document.createElement("div");
+        listItem.appendChild(iconDiv);
+        iconDiv.style.display = "flex";
+        iconDiv.style.alignItems = "center";
+
+        const editIcon = document.createElement("img");
+        editIcon.src = "images/edit.png";
+        editIcon.style.width = "24px";
+        editIcon.style.height = "24px";
+        iconDiv.appendChild(editIcon);
+
+        const deleteIcon = document.createElement("img");
+        deleteIcon.src = "images/delete.png";
+        deleteIcon.style.width = "24px";
+        deleteIcon.style.height = "24px";
+        deleteIcon.style.marginLeft = "5px";
+        deleteIcon.style.marginRight = "5px";
+        iconDiv.appendChild(deleteIcon);
+
+        const shareIcon = document.createElement("img");
+        shareIcon.src = "images/share.png";
+        shareIcon.style.width = "24px";
+        shareIcon.style.height = "24px";
+        iconDiv.appendChild(shareIcon);
+
+        const line = document.createElement('hr');
+        bookmarkList.appendChild(line);
       } else {
         console.error("[오류] 현재 시간을 가져오는 데에 실패함");
       }
@@ -29,6 +89,6 @@ function getYouTubeVideoCurrentTime(button) {
 document.addEventListener('DOMContentLoaded', function() {
   var addButton = document.getElementById('btn');
   addButton.addEventListener('click', function() {
-    getYouTubeVideoCurrentTime();
+    getVideoCurrentTime();
   });
 });
