84ca8e50fec13fc021a1e4d5a265324eaa63bd45
김애리 <zjadofl0408@gmail.com>
2024-05-10 01:55:09 +0900

    [refactor] 북마크추가와 재생시간 함수 분리

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// 영상 시간(초)를 형식화
 function formatVideoTime(videoSeconds) {
   const hours = Math.floor(videoSeconds / 3600);
   const minutes = Math.floor((videoSeconds % 3600) / 60);
@@ -10,80 +11,51 @@ function formatVideoTime(videoSeconds) {
   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 }
 
-function getVideoCurrentTime(button) {
-  chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
-    let results = await chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: () => {
-        const video = document.querySelector('video');
-        if (video) {
-          return parseInt(video.currentTime);
-        } else {
-          console.error("[오류] Video 요소를 찾을 수 없음");
-          return null;
-        }
-      }
-    });
-
-    const bookmarkList = document.getElementById('bookmark_list');
-    for (let result of results) {
-      if (result.result !== null) {
-        const listItem = document.createElement('li');
-        listItem.style.display = "flex";
-        bookmarkList.appendChild(listItem);
-        listItem.scrollIntoView({ behavior: 'smooth', block: 'end' });
-
-        const videoInfoDiv = document.createElement("div");
-        listItem.appendChild(videoInfoDiv);
-
-        const memo = document.createElement('input');
-        memo.type = "text";
-        memo.placeholder = "빈 메모입니다.";
-        memo.disabled = true;
-        memo.style.paddingBottom = "10px";
-        memo.style.border = "none";
-        memo.style.background = "transparent";
-        videoInfoDiv.appendChild(memo);
-
-        const time = document.createElement("span");
-        time.textContent = `${formatVideoTime(result.result)}`;
-        time.style.color = "#b8860b";
-        time.style.borderWidth = "2px";
-        time.style.fontSize = "14px";
-        videoInfoDiv.appendChild(time);
-
-        const iconDiv = document.createElement("div");
-        listItem.appendChild(iconDiv);
-        iconDiv.style.display = "flex";
-        iconDiv.style.alignItems = "center";
-
-        const editIcon = document.createElement("img");
-        editIcon.src = "images/edit.png";
-        editIcon.style.width = "24px";
-        editIcon.style.height = "24px";
-        iconDiv.appendChild(editIcon);
-
-        const deleteIcon = document.createElement("img");
-        deleteIcon.src = "images/delete.png";
-        deleteIcon.style.width = "24px";
-        deleteIcon.style.height = "24px";
-        deleteIcon.style.marginLeft = "5px";
-        deleteIcon.style.marginRight = "5px";
-        iconDiv.appendChild(deleteIcon);
-
-        const shareIcon = document.createElement("img");
-        shareIcon.src = "images/share.png";
-        shareIcon.style.width = "24px";
-        shareIcon.style.height = "24px";
-        iconDiv.appendChild(shareIcon);
-
-        const line = document.createElement('hr');
-        bookmarkList.appendChild(line);
-      } else {
-        console.error("[오류] 현재 시간을 가져오는 데에 실패함");
-      }
+//현재 재생시간 가져오기
+async function getVideoCurrentTime() {
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  const tabId = tabs[0].id;
+  const results = await chrome.scripting.executeScript({
+    target: {tabId: tabId},
+    function: () => {
+      const video = document.querySelector('video');
+      return video ? parseInt(video.currentTime) : null;
     }
   });
+
+  const bookmarkList = document.getElementById("bookmark_list");
+  for (let result of results) {
+    if (result.result != null) {
+      addBookmark(result.result);
+    } else {
+      console.error("[오류] Video 요소를 찾을 수 없음");
+    }
+  }
+}
+
+// 북마크 추가
+function addBookmark(videoSeconds) {
+  const listItem = document.createElement("li");
+  listItem.style.padding = "5px";
+  listItem.style.display = "flex"; 
+  listItem.classList.add('bookmark-item');
+  listItem.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  listItem.innerHTML = `
+  <div class="video-info">
+    <input type="text" class="memo" placeholder="빈 메모입니다." disabled >
+    <span class="video-time">${formatVideoTime(videoSeconds)}</span>
+  </div>
+  <div class="icon-div">
+    <img src="images/edit.png" class="edit-icon">
+    <img src="images/delete.png" class="delete-icon">
+    <img src="images/share.png" class="share-icon">
+  </div>
+  `;
+  const bookmarkList = document.getElementById('bookmark_list');
+  bookmarkList.appendChild(listItem);
+  listItem.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  const hr = document.createElement("hr");
+  bookmarkList.appendChild(hr);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
